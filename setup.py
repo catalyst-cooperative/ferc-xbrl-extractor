7377e02e4f43999a829e1cc50d9235c834be467b
zschira <zach.schira@catalyst.coop>
2022-07-28 16:50:57 -0400

    Update Arelle dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     project_urls={
         "Source": "https://github.com/catalyst-cooperative/ferc-xbrl-extract",
         "Documentation": "https://PACKAGE_NAME.readthedocs.io",
-        "Issue Tracker": "https://github.com/catalyst-cooperative/xbrl-extract/issues",
+        "Issue Tracker": "https://github.com/catalyst-cooperative/ferc-xbrl-extract/issues",
     },
     keywords=["xbrl", "ferc", "financial"],
     python_requires=">=3.8,<3.11",
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         "pydantic>=1.9,<2",
         "coloredlogs~=15.0",
-        "w-versioned-arelle @ git+https://github.com/Arelle/Arelle.git@0.9.91",
+        "catalystcoop.arelle-mirror~=1.2.1",
         "sqlalchemy>=1.4,<2",
         "pandas>=1.4,<1.5",
         "stringcase~=1.2.0",
