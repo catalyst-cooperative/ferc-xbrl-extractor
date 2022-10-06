fc609b8184d63d4e15a1ab5ace20ea2486d01cd9
zschira <zach.schira@catalyst.coop>
2022-10-06 15:37:41 -0400

    Fix empty dimension value handling

--- src/ferc_xbrl_extractor/instance.py
+++ src/ferc_xbrl_extractor/instance.py
@@ -80,7 +80,7 @@ class Axis(BaseModel):
             dim = elem.getchildren()[0]
             return cls(
                 name=elem.attrib["dimension"],
-                value=dim.text,
+                value=dim.text if dim.text else "",
                 dimension_type=DimensionType.TYPED,
             )
 
