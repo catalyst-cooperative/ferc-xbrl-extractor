2b744c5807eb4ce33288a87a03ffa89eca80ad72
zschira <zach.schira@catalyst.coop>
2022-12-20 16:16:25 -0500

    Add integration test to run example CLI command

--- tests/integration/console_scripts_test.py
+++ tests/integration/console_scripts_test.py
@@ -13,10 +13,34 @@ ENTRY_POINTS = [
 
 @pytest.mark.parametrize("ep", ENTRY_POINTS)
 @pytest.mark.script_launch_mode("inprocess")
-def test_pudl_scripts(script_runner, ep):
+def test_extractor_scripts(script_runner, ep):
     """Run each deployed console script with --help for testing.
 
     The script_runner fixture is provided by the pytest-console-scripts plugin.
     """
     ret = script_runner.run(ep, "--help", print_result=False)
     assert ret.success  # nosec: B101
+
+
+@pytest.mark.script_launch_mode("inprocess")
+def test_extract_example_filings(script_runner):
+    """Test the XBRL extraction on the example filings.
+
+    Run the example CLI commands from the readme, and verify that they all return
+    successfully.
+    """
+    ret = script_runner.run(
+        "xbrl_extract",
+        "examples/ferc1-2021-sample.zip",
+        "./ferc1-2021-sample.sqlite",
+        "--taxonomy",
+        "examples/taxonomy.zip",
+        "--archive-path",
+        "taxonomy/form1/2021-01-01/form/form1/form-1_2021-01-01.xsd",
+        "--metadata-path",
+        "metadata.json",
+        "--save-datapackage",
+        "datapackage.json",
+    )
+
+    assert ret.success
